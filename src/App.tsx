db11501b4aa44c82761f58a20c507516092f1eb6
sumit0527 <117132776+sumit0527@users.noreply.github.com>
2026-06-20 20:56:52 +0530

    Refactor App component structure and remove ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -2,40 +2,32 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import IntersectObserver from '@/components/common/IntersectObserver';
 import { Toaster } from '@/components/ui/sonner';
-import { ScrollToTop } from '@/components/ScrollToTop';
+import { AuthProvider } from '@/features/auth/hooks/AuthContext';
 
 import { routes } from './routes';
-import { AuthProvider } from '@/contexts/AuthContext';
-import { ThemeProvider } from '@/contexts/ThemeContext';
-import { RouteGuard } from '@/components/common/RouteGuard';
 
 const App: React.FC = () => {
   return (
-    <ThemeProvider>
-      <Router>
-        <AuthProvider>
-          <RouteGuard>
-            <IntersectObserver />
-            <div className="flex min-h-screen flex-col">
-              <main className="flex-grow">
-                <Routes>
-                  {routes.map((route, index) => (
-                    <Route
-                      key={index}
-                      path={route.path}
-                      element={route.element}
-                    />
-                  ))}
-                  <Route path="*" element={<Navigate to="/" replace />} />
-                </Routes>
-              </main>
-            </div>
-            <Toaster />
-            <ScrollToTop />
-          </RouteGuard>
-        </AuthProvider>
-      </Router>
-    </ThemeProvider>
+    <Router>
+      <AuthProvider>
+        <IntersectObserver />
+        <div className="flex flex-col min-h-screen">
+          <main className="flex-grow">
+            <Routes>
+              {routes.map((route, index) => (
+                <Route
+                  key={index}
+                  path={route.path}
+                  element={route.element}
+                />
+              ))}
+              <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+          </main>
+        </div>
+        <Toaster />
+      </AuthProvider>
+    </Router>
   );
 };
 
